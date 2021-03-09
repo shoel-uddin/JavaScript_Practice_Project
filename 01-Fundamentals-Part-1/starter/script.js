5b5bcb556d521ea10a974aafd54ec6a40b0d70f7
Shoel Uddin <shoeluddin@Shoels-MBP.attlocal.net>
2021-03-09 10:09:46 -0500

    fundamentals-01

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let js = 'amazing';
+      //if (js === 'amazing') alert ('JS is fun');
+
+console.log(10 + 5 + 10 - 5); 
+
+console.log('Sho'); 
+
+let firstName = "Sho"
+console.log(firstName);
+
+/// In JS variables need to be in camelCase( first letter lowercase and any word after with upperCase. i.e. firstName. else use underscore, i.e. first_name)
+
+//Using 'Typeof'
+let sho = true;
+console.log(typeof true );
+// Logs as "Boolean"
+console.log(typeof  sho);
+// Logs as "Boolean" since 'sho = true'
+console.log(typeof 30 );
+// Logs as "Number"
+console.log(typeof 'Sho' );
+// Logs as "String"
+
+sho = 'YES'
+// Since 'sho' was assigned a variable once it can be assinged a new variable without using 'let'
